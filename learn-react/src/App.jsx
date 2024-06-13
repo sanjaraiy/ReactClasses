76a37a89b065d3ee9a3ffa0f35e0a124301eb454
sanjaraiy <deepakrai78788@gmail.com>
2024-06-13 11:38:19 +0530

    react-tasks

---
+++ learn-react/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  const username="Sanjh"
+
+  return (
+    <>
+      <div> hello everyone </div>
+      <h1> Name : {username} </h1>
+    </>
+  )
+}
+
+export default App
---
+++ learn-react/src/main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//================ Task 1 ===========================
+// function MyApp(){
+//   return (
+//      <>
+//       <div>Hello Dosto</div>
+//      </>
+//   )
+// }
+
+//============== Task 2 ============================
+// const ReactElement = {
+//   type : 'a',
+//   props : {
+//      href : 'https://google.com',
+//      target : '_blank'
+//   },
+//   children : 'Click me to visit google'
+// }
+
+//============= Task 3 ============================
+// const anotheruser = "chai or code";
+// const reactElement = React.createElement(
+//   'a',
+//   {
+//     href : 'https://google.com', 
+//     target : '_blank'
+//   },
+//   'click me to visit google',
+//    anotheruser
+// )
+
+//=============== Task 4 ========================
+// const anotherElement = (<a href='https://google.com' target='_blank'>Visit Google</a>)
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    // MyApp()
+    // ReactElement
+    //  reactElement 
+    // anotherElement
+    // <App></App>
+  
+)
